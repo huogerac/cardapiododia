e8188174937c56be607e7c70ee8f9dc2136faaa2
Roger Camargo <huogerac@gmail.com>
2012-08-30 07:57:47 -0300

    A pasta de templates foi movida para o local correto

--- django/cardapiododia/settings.py
+++ django/cardapiododia/settings.py
@@ -122,10 +122,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+    # django admin
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    # cardapio do dia
     'votacao',
 )
 
