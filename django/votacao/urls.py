b54cc2acdec18726dcb83e8b89c78d536f8828d0
Roger Camargo <huogerac@gmail.com>
2012-08-31 01:15:58 -0300

    nova pagina de exibir a opções para votação

--- django/votacao/urls.py
+++ django/votacao/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, url
 
-from .views import votacao, criar_votacao
+from .views import votacao, criar_votacao, opcoes_votacao
 
 urlpatterns = patterns('votacao.views',
 
     #votacao    
     url(r'^votacao/nova', criar_votacao, name='url_criar_votacao'),
+    
+    url(r'^votacao/(\d+)', opcoes_votacao, name='url_opcoes_votacao'),
+
     url(r'^votacao/', votacao, name='url_votacao'),
     
 )
--- django/votacao/views.py
+++ django/votacao/views.py
@@ -1,13 +1,36 @@
 # Create your views here.
 from django.shortcuts import render
 
-from .models import DIAS
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+
+from .models import DIAS, Votacao, Cardapio
 
 def votacao(request):
     template_name = 'votacao/home.html'
     return render(request, template_name)
     
+class VotacaoForm(forms.ModelForm):
+    class Meta:
+        model = Votacao
+            
 def criar_votacao(request):
-    queryset = [dict(id=id, nome=nome) for id, nome in DIAS]
-    template_name='votacao/criarvotacao.html'
-    return render(request, template_name, {'object_list': queryset})
+
+    if request.POST:
+        form = VotacaoForm(request.POST)
+        if form.is_valid():
+            votacao = form.save()
+            return HttpResponseRedirect('/votacao/' + str(votacao.id))
+    else:
+        form = VotacaoForm()
+    
+    payload = {'form':form }
+    return render(request, 'votacao/criarvotacao.html', payload) 
+
+def opcoes_votacao(request, id_votacao):
+    votacao = get_object_or_404(Votacao, pk=id_votacao)
+    cardapios = Cardapio.objects.all()
+    return render(request, 'votacao/opcoesvotacao.html', {'votacao': votacao, 'cardapio_list': cardapios})
+    
+    
