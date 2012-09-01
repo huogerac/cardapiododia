2940d51f0f29e1fd6ee56510f78e91e2d62cd4d0
Roger Camargo <huogerac@gmail.com>
2012-09-01 14:42:30 -0300

    adicionado os campos hora do almoço e votação encerrada na tabela Votação

--- django/votacao/models.py
+++ django/votacao/models.py
@@ -1,6 +1,8 @@
+# coding=utf-8
+
 from django.db import models
 
-DIAS = [
+DIAS_DA_SEMANA = [
     ('segunda', 'Segunda-Feira'),
     ('terca', 'Terca-Feira'),
     ('quarta', 'Quarta-Feira'),
@@ -18,7 +20,7 @@ class Restaurante(models.Model):
         
 class Cardapio(models.Model):
     restaurante = models.ForeignKey(Restaurante)
-    dia = models.CharField(max_length=32, choices=DIAS)
+    dia = models.CharField(max_length=32, choices=DIAS_DA_SEMANA)
     cardapioDia = models.FileField(upload_to='cardapio')
 
     def __unicode__(self):
@@ -27,7 +29,9 @@ class Cardapio(models.Model):
 class Votacao(models.Model):
     data = models.DateTimeField(auto_now_add=True)
     nome = models.CharField(max_length=64)
-    dia = models.CharField(max_length=32, choices=DIAS)
+    dataDoAlmoco = models.DateTimeField(u'Data do almoço')
+    diaDaSemana = models.CharField(max_length=32, choices=DIAS_DA_SEMANA)
+    encerrada = models.BooleanField(default=False)
 	
     def __unicode__(self):
         return self.nome
--- django/votacao/views.py
+++ django/votacao/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 
 from django import forms
 from django.http import HttpResponseRedirect, HttpResponse
+from django.forms.widgets import HiddenInput
 from django.shortcuts import render, render_to_response, get_object_or_404
 
-from .models import DIAS, Votacao, Cardapio
+from datetime import date, datetime, time
+
+from .models import Votacao, Cardapio
 
 def votacao(request):
     template_name = 'votacao/home.html'
@@ -26,14 +29,20 @@ def criar_votacao(request):
             votacao = form.save()
             return HttpResponseRedirect('/votacao/' + str(votacao.id))
     else:
-        form = VotacaoForm()
+
+        hoje = date.today()
+        meiodia = datetime.combine(hoje, time(12, 00)) 
+        nova_votacao = Votacao(dataDoAlmoco=meiodia)
+        
+        form = VotacaoForm(instance=nova_votacao)
+        form.fields['encerrada'].widget = HiddenInput()
     
     payload = {'form':form }
     return render(request, 'votacao/criarvotacao.html', payload) 
 
 def opcoes_votacao(request, id_votacao):
     votacao = get_object_or_404(Votacao, pk=id_votacao)
-    cardapios = Cardapio.objects.filter(dia=votacao.dia)
+    cardapios = Cardapio.objects.filter(dia=votacao.diaDaSemana)
     return render(request, 'votacao/opcoesvotacao.html', {'votacao': votacao, 'cardapio_list': cardapios})
     
     
