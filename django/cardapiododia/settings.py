6ee3e3a2e03e87bc19ace1390cf136b91a31e0d0
Roger Camargo <huogerac@gmail.com>
2012-09-19 18:44:18 -0300

    Suporte a base de dados PostgreSQL

--- django/cardapiododia/settings.py
+++ django/cardapiododia/settings.py
@@ -14,17 +14,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cardapiododia-db.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -128,6 +117,8 @@ INSTALLED_APPS = (
     # cardapio do dia
     'votacao',
 )
+
+execfile(os.path.join(PROJECT_PATH,'settings_local.include'))
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
